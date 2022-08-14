f7734b85c3816b24ab71e5855d724330b9d5e687
Himezakura <89678734+Himezakura@users.noreply.github.com>
2022-08-14 16:34:42 +0900

    Update cheats.cpp

--- Sources/cheats.cpp
+++ Sources/cheats.cpp
@@ -47,9 +47,9 @@ void kantu(MenuEntry *entry){
   if (B == 0) {
    Process::Write32(0x0718C84, 0xE1A01005);
   } else if (B == 1) {
-   Process::Write32(0x0718C84, 0xE1A01007);
-  } else if (B == 2) {
    Process::Write32(0x0718C84, 0xE1A01002);
+  } else if (B == 2) {
+   Process::Write32(0x0718C84, 0xE1A01007);
 }}
 
 
@@ -102,6 +102,176 @@ void block_wanpan_turuhasi(MenuEntry *entry){
   } else if (B == 1) {
    Process::Write32(0x071B60C, 0x3F800000);
 }}
+
+
+//時間逆転
+void jikan_gyaku(MenuEntry *entry){
+	Keyboard key("朝なら夜に、夜なら朝になります(少し不安定)", {"オン", "オフ"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x0739640, 0xBF800000);
+  } else if (B == 1) {
+   Process::Write32(0x0739640, 0x3F800000);
+}}
+
+
+//世界透明化
+void sekai_toumei(MenuEntry *entry){
+	Keyboard key("全ブロック透明化（チェストとかはならない）", {"オン", "オフ"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x074F964, 0x00000000);
+  } else if (B == 1) {
+   Process::Write32(0x074F964, 0x3F800000);
+}}
+
+
+//時間変更（朝、夜）
+void jikan_henkou(MenuEntry *entry){
+	Keyboard key("時間変更（少し不安定）", {"朝", "夜", "オフ"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x0739638, 0x3E800000);
+  } else if (B == 1) {
+   Process::Write32(0x0739638, 0xBE800000);
+  } else if (B == 2) {
+   Process::Write32(0x0739638, 0x00000000);
+}}
+
+
+//世界透明化
+void sekai_toumei(MenuEntry *entry){
+	Keyboard key("雨が降っているときに有効", {"オン", "オフ"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x0649690, 0x00000000);
+  } else if (B == 1) {
+   Process::Write32(0x0649690, 0x3F800000);
+}}
+
+
+//雷降らす（強制）
+void kaminari(MenuEntry *entry){
+	Keyboard key("雨が降っているときに有効", {"雷降らす（強制）", "雷降らす（強制） オフ", "雷降った時のact固定",　"雷降った時のact固定　オフ"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x064968C, 0xE1A00000);
+  } else if (B == 1) {
+   Process::Write32(0x064968C, 0x3C23D70A);
+  } else if (B == 2) {
+   Process::Write32(0x0649680, 0xC2400000);
+  } else if (B == 3) {
+   Process::Write32(0x0649680, 0x3F800001);
+}}
+
+
+//霧の色変更
+void kiri_color(MenuEntry *entry){
+	Keyboard key("霧の色を選択", {"赤", "青", "黄色",　"緑", "紫", "白", "黒", "オフ"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x07393C4, 0x00000000);
+   Process::Write32(0x07393C8, 0x40000000);
+   Process::Write32(0x07393CC, 0x00000000);
+  } else if (B == 1) {
+   Process::Write32(0x07393C4, 0x40000000);
+   Process::Write32(0x07393C8, 0x00000000);
+   Process::Write32(0x07393CC, 0x00000000);
+  } else if (B == 2) {
+   Process::Write32(0x07393C4, 0x00000000);
+   Process::Write32(0x07393C8, 0x40000000);
+   Process::Write32(0x07393CC, 0x40000000);
+  } else if (B == 3) {
+   Process::Write32(0x07393C4, 0x00000000);
+   Process::Write32(0x07393C8, 0x00000000);
+   Process::Write32(0x07393CC, 0x40000000);
+　　}　else if (B == 4) {
+   Process::Write32(0x07393C4, 0x40000000);
+   Process::Write32(0x07393C8, 0x40000000);
+   Process::Write32(0x07393CC, 0x00000000);
+  } else if (B == 5) {
+   Process::Write32(0x07393C4, 0x00000000);
+   Process::Write32(0x07393C8, 0x70000000);
+   Process::Write32(0x07393CC, 0x00000000);
+  } else if (B == 6) {
+   Process::Write32(0x07393C4, 0x00000000);
+   Process::Write32(0x07393C8, 0x400000000);
+   Process::Write32(0x07393CC, 0x00000000);
+  } else if (B == 7) {
+   Process::Write32(0x07393C4, 0x3F800000);
+   Process::Write32(0x07393C8, 0x3F2BABAC);
+   Process::Write32(0x07393CC, 0x3F52D2D4);
+}}
+
+
+//アイテムの個数文字真ん中
+void item_slot_suuji(MenuEntry *entry){
+	Keyboard key("アイテムの個数文字が真ん中に配置される（ゴミコード）", {"オン", "オフ"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x01ED2AC, 0x40000000);
+  } else if (B == 1) {
+   Process::Write32(0x01ED2AC, 0x3F800000);
+}}
+
+
+//スロットアイテム着色
+void item_slot_color(MenuEntry *entry){
+	Keyboard key("スロットアイテムのみ黒くなる", {"オン", "オフ"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x01ED0D8, 0x00000000);
+  } else if (B == 1) {
+   Process::Write32(0x01ED0D8, 0x3F800000);
+}}
+
+
+//スロットアイテム大きさ変更
+void item_slot_ookisa(MenuEntry *entry){
+	Keyboard key("スロットアイテムのみ大きさ変更", {"大きい", "通常", "小さい"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x01ED0D4, 0x40B00000);
+  } else if (B == 1) {
+   Process::Write32(0x01ED0D4, 0x40000000);
+　　} else if (B == 2) {
+   Process::Write32(0x01ED0D4, 0x3F500000);
+}}
+
+
+//プライヤー透明化
+void player_toumei(MenuEntry *entry){
+	Keyboard key("", {"オン", "オフ"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x019D320, 0x10000000);
+   Process::Write32(0x019D324, 0x90000000);
+  } else if (B == 1) {
+   Process::Write32(0x019D320, 0x3F800000);
+   Process::Write32(0x019D324, 0xBF800000);
+}}
+
+
+//プライヤー発光
+void player_hakkou(MenuEntry *entry){
+	Keyboard key("", {"白", "通常", "黒"});
+ int B = key.Open();
+  if (B == 0) {
+   Process::Write32(0x01D9868, 0x7FFFFFFF);
+  } else if (B == 1) {
+   Process::Write32(0x01D9868, 0x3F800000);
+　　} else if (B == 2) {
+   Process::Write32(0x01D9868, 0x00000000);
+}}
+
+
+
+
+
+
+
+
+
 
 
 
